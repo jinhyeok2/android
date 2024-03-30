e1c10b27def6e2762c175efa281ff0e978799aec
최진혁 <37572559+jinhyeok2@users.noreply.github.com>
2024-03-30 14:42:17 +0900

    Add files via upload

---
+++ 0318/주사위/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.dice;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView dice;
+    Button roll;
+    Random random = new Random();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        dice = (ImageView)findViewById(R.id.diceA);
+        roll = findViewById(R.id.rollDice);
+    }
+    public void roll(View view) {
+        int count = random.nextInt(6) + 1;
+
+        switch (count) {
+            case 1:
+                dice.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                dice.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                dice.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                dice.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                dice.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                dice.setImageResource(R.drawable.dice6);
+                break;
+        }
+    }
+}
